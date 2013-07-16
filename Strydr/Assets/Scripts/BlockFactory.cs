6a0bf34de769c7b76291a956c66e3dfbc6dd94fc
sacredgeometry <lostrecordsu@gmail.com>
2013-07-16 23:32:02 +0100

    EOD Commit

--- Strydr/Assets/Scripts/BlockFactory.cs
+++ Strydr/Assets/Scripts/BlockFactory.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class BlockFactory : MonoBehaviour {
-
+	
+	Dictionary<Vector2, GameObject> Blocks = new Dictionary<Vector2, GameObject>();
+	
+	public GameObject[] BlockLibrary; 
+	
+	public int levelWidth, levelHeight;
+	
 	// Use this for initialization
 	void Start () {
+		for (int x = 0; x < levelWidth; x++) {
+			for (int y = 0; y < levelHeight; y++) {
+				Instantiate((Object)BlockLibrary[0], new Vector3(x,y,0), Quaternion.identity);
+			}
+		}
 	
 	}
 	
